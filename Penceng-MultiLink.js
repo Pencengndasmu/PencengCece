751c3c0928c3b9db35f07ebb33c40735a29ad7ec
Pencengndasmu <30433426+Pencengndasmu@users.noreply.github.com>
2017-07-27 13:06:14 +0700

    Add files via upload

--- Penceng-MultiLink.js
+++ Penceng-MultiLink.js
@@ -19,13 +19,23 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*15)+ 1);
 var xxx=null;
 if(x=="1"){xxx="12-benefits-of-chia-seed-for-body.html"}
 if(x=="2"){xxx="17-benefits-and-benefits-of-belimbing.html"}
 if(x=="3"){xxx="causes-of-early-menopause-and-how-to.html"}
 if(x=="4"){xxx="how-to-treat-vitiligo-disease-causes.html"}
-if(x=="5"){xxx="32-ways-to-increase-appetite-for-adults.html"}a_to_ve[i].href="https://pencengcece.blogspot.com/2017/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="32-ways-to-increase-appetite-for-adults.html"}
+if(x=="6"){xxx="20-ways-to-take-care-of-face.html"}
+if(x=="7"){xxx="17-benefits-of-black-seed-for-diet.html"}
+if(x=="8"){xxx="22-benefits-of-ginger-water-lose-weight.html"}
+if(x=="9"){xxx="12-benefits-of-goat-milk-soap-for-skin.html"}
+if(x=="10"){xxx="26-benefits-of-lime-for-diet.html"}
+if(x=="11"){xxx="17-symptoms-of-cervical-cancer.html"}
+if(x=="12"){xxx="20-benefits-of-banana-heart-for-health.html"}
+if(x=="13"){xxx="14-benefits-of-red-onions-for-health.html"}
+if(x=="14"){xxx="19-dutch-eggplant-effects-for-health.html"}
+if(x=="15"){xxx="17-benefits-princess-shame-for-body.html"}a_to_ve[i].href="https://pencengcece.blogspot.com/2017/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
